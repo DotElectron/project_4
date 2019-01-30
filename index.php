6f2f9acc35fefb54add87b2596a203c969de27ee
DotElectron <baal_therm.electron@live.fr>
2019-01-30 13:09:13 +0100

    0.0.1.1 - Update index.php (font)
    Added in header :
    - Missed link of google font

--- index.php
+++ index.php
@@ -19,6 +19,8 @@
     <!-- Favicons -->
     <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
     <link rel="icon" href="favicon.png" type="image/png">
+    <!-- Font Links -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Anonymous+Pro:400,700">
     <!-- CSS Links -->
     <link rel="stylesheet" type="text/css" href="public/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
