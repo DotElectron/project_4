d35cb1a5aa74f70c4c4095a5a52069af798d3787
DotElectron <baal_therm.electron@live.fr>
2019-02-04 11:58:14 +0100

    Error management
    - index.php: debug-area - new tests...
    - css : debug version - enlarge...
    - Error_manager.php : class

---
+++ models/Error_manager.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Rochefort\Classes;
+
+class Error_manager
+{
+	private function __construct() {}		//Force no instanciation...
+
+	private static $err;
+	public static function setErr($_err) 
+	{
+		self::$err .= '<div>' . $_err . '</div>';
+	}
+	private static function getErr() 
+	{
+		return (self::$err);
+	}
+	private static function clearErr() 
+	{
+		self::$err = null;
+	}
+
+	/**
+	* Return the current errors list...
+	* @return String of cumulated errors
+	*/
+	public static function displayErr() 
+	{
+		if (self::$err !== null)
+		{
+			echo self::getErr();
+			self::clearErr();
+		}
+	}
+	/**
+	* [External test of: displayErr]					[0.0.5.2 PASSED]
+	* Conditions: ---
+	*
+	* require_once('models/Error_manager.php');
+	* use Rochefort\Classes\Error_manager;
+	*
+	* Error_manager::setErr('BLANK TEST: is a valid test !');
+	* Error_manager::displayErr();
+	*/
+}
+
+?>
