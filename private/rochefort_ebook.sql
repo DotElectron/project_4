6eff343ee40970051492306ea421e1785ab0943d
DotElectron <baal_therm.electron@live.fr>
2019-02-11 15:31:50 +0100

    Update rochefort_ebook.sql
    - com_author in comments table (NN)

--- private/rochefort_ebook.sql
+++ private/rochefort_ebook.sql
@@ -33,28 +33,6 @@ CREATE TABLE `chapters` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `comments`
---
-
-DROP TABLE IF EXISTS `comments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `comments` (
-  `com_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique ID of the comment (as P_KEY)',
-  `com_author` varchar(60) DEFAULT 'Anonyme' COMMENT 'Name of the author of the comment',
-  `com_text` text NOT NULL COMMENT 'Html content of the comment',
-  `com_flag` int(11) NOT NULL COMMENT 'The flag counter (from users)',
-  `com_muted` tinyint(1) NOT NULL COMMENT 'The muted status (from admin)',
-  `com_modifier` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'TimeStamp of the last modification (as Index)',
-  `com_part_id` int(11) DEFAULT NULL COMMENT 'ID from the parent part (as Foreign)',
-  PRIMARY KEY (`com_id`),
-  KEY `IDX_modifier` (`com_modifier`),
-  KEY `IDX_part_id` (`com_part_id`),
-  CONSTRAINT `F_KEY_part_id` FOREIGN KEY (`com_part_id`) REFERENCES `parts` (`part_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Contains definition of the part''s comment, need to be treated by DESC Timestamp.';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Table structure for table `parts`
 --
 
@@ -77,6 +55,28 @@ CREATE TABLE `parts` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comments` (
+  `com_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique ID of the comment (as P_KEY)',
+  `com_author` varchar(60) NOT NULL DEFAULT 'Guest' COMMENT 'Name of the author of the comment',
+  `com_text` text NOT NULL COMMENT 'Html content of the comment',
+  `com_flag` int(11) NOT NULL DEFAULT '0' COMMENT 'The flag counter (from users)',
+  `com_muted` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'The muted status (from admin)',
+  `com_modifier` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'TimeStamp of the last modification (as Index)',
+  `com_part_id` int(11) DEFAULT NULL COMMENT 'ID from the parent part (as Foreign)',
+  PRIMARY KEY (`com_id`),
+  KEY `IDX_modifier` (`com_modifier`),
+  KEY `IDX_part_id` (`com_part_id`),
+  CONSTRAINT `F_KEY_part_id` FOREIGN KEY (`com_part_id`) REFERENCES `parts` (`part_id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Contains definition of the part''s comment, need to be treated by DESC Timestamp.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
@@ -85,4 +85,4 @@ CREATE TABLE `parts` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-02-08  8:58:08
+-- Dump completed on 2019-02-11 15:30:18
