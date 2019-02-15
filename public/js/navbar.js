c3413e5e62190386b69b7790f422347d695a4e9f
DotElectron <baal_therm.electron@live.fr>
2019-02-15 18:45:12 +0100

    Create navbar.js
    JS: switch between form, display or not (auto-correction versus)

---
+++ public/js/navbar.js
@@ -0,0 +1,65 @@
+// --------------------------
+// --------------------------
+// Navbar switch to connect...
+function switch_connection()
+{
+	var blockConnect = document.getElementById("connect");
+	if (blockConnect !== null)
+	{
+		if (blockConnect.style.display === ""
+			|| blockConnect.style.display === "none")
+		{
+			blockConnect.style.display = "block";
+			hasRegistration();
+		}
+		else
+		{
+			blockConnect.style.display = "none";
+		}
+		blockConnect = null;
+	}
+}
+function hasConnection()
+{
+	var blockConnect = document.getElementById("connect");
+	if (blockConnect !== null)
+	{
+		if (blockConnect.style.display = "block")
+		{
+			blockConnect.style.display = "none";
+		}
+		blockConnect = null;
+	}
+}
+
+// Navbar switch to register...
+function switch_registration()
+{
+	var blockRegister = document.getElementById("register");
+	if (blockRegister !== null)
+	{
+		if (blockRegister.style.display === ""
+			|| blockRegister.style.display === "none")
+		{
+			blockRegister.style.display = "block";
+			hasConnection();
+		}
+		else
+		{
+			blockRegister.style.display = "none";
+		}
+		blockRegister = null;
+	}
+}
+function hasRegistration()
+{
+	var blockRegister = document.getElementById("register");
+	if (blockRegister !== null)
+	{
+		if (blockRegister.style.display = "block")
+		{
+			blockRegister.style.display = "none";
+		}
+		blockRegister = null;
+	}
+}
