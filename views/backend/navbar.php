92f131488b7ce525fde03d0d7d2a0c682fb1cbfb
DotElectron <baal_therm.electron@live.fr>
2019-02-15 18:42:24 +0100

    Create navbar.php
    (backend: goto admin pages)

---
+++ views/backend/navbar.php
@@ -0,0 +1,16 @@
+<!-- HTML - NavBar treatment -->
+<div>
+	<samp>Administrer :</samp>
+	<!-- Admin controls -->
+	<ul>
+		<li>
+			<a href="?uChap">Gérer les chapitres</a>
+		</li>
+		<li>
+		<a href="?uPart">Rédiger les épisodes</a>
+		</li>
+		<li>
+			<a href="?uComm">Modérer les commentaires</a>
+		</li>
+	</ul>
+</div>
