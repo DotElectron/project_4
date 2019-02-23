88240f09e8affc3bfbb0af16c61394d71c91c438
DotElectron <baal_therm.electron@live.fr>
2019-02-23 12:55:10 +0100

    Corrective(bis): admin_chapter.php
    Prevent numeric request on title

--- controllers/admin_chapters.php
+++ controllers/admin_chapters.php
@@ -18,31 +18,45 @@ if (isset($_POST['admLastData']))
 		// An (title) update request was sent...
 		$chapterClass = new PDO_chapter($_POST['admLastData']);
 		if (isset($activeDebug)) { Error_manager::setErr('Admin: get chapter < ' . $chapterClass->getTitle() . ' (' . $chapterClass->getOrder() . ')'); }
+		//Prepare data...
+		$admChapter = htmlspecialchars($_POST['admChapter']);
+		if (is_numeric($admChapter))
+		{
+			$admChapter .= '¤';
+		}
 		if ($chapterClass->getId()
 			&& $chapterClass->updateChapter($chapterClass->getId(), 
 										 	$chapterClass->getOrder(), 
-										 	htmlspecialchars($_POST['admChapter'])))
+										 	$admChapter))
 		{
-			$chapterClass = new PDO_chapter(htmlspecialchars($_POST['admChapter']));
+			$chapterClass = new PDO_chapter($admChapter);
 			if (isset($activeDebug)) { Error_manager::setErr('Admin: update chapter >>> ' . $chapterClass->getTitle()); }
 			else { Error_manager::setErr('Chapitre actualisé : "' . $chapterClass->getTitle() . '"'); }
 		}
 		elseif (isset($activeDebug)) { Error_manager::setErr('Admin: failed to update chapter ! [' . $chapterClass->getTitle() . ']'); }
 		else { Error_manager::setErr('Actualisation impossible depuis : "' . htmlspecialchars($_POST['admLastData']) . '"'); }
+		$admChapter = null;
 	}
 	else if (isset($_POST['admNewChapter']))
 	{
 		// A creation request was sent...
-		$chapterClass = new PDO_chapter(' ');
-		if ($chapterClass->createChapter($_POST['admLastData'], 
-										 htmlspecialchars($_POST['admNewChapter'])))
+		$chapterClass = new PDO_chapter('¤');
+		//Prepare data...
+		$admNewChapter = htmlspecialchars($_POST['admNewChapter']);
+		if (is_numeric($admNewChapter))
 		{
-			$chapterClass = new PDO_chapter(htmlspecialchars($_POST['admNewChapter']));
+			$admNewChapter .= '¤';
+		}
+		if ($chapterClass->createChapter($_POST['admLastData'], 
+										 $admNewChapter))
+		{
+			$chapterClass = new PDO_chapter($admNewChapter);
 			if (isset($activeDebug)) { Error_manager::setErr('Admin: create chapter >>> ' . $chapterClass->getTitle() . ' (' . $chapterClass->getOrder() . ')'); }
 			else { Error_manager::setErr('Chapitre créé : "' . $chapterClass->getTitle() . '"'); }
 		}
 		elseif (isset($activeDebug)) { Error_manager::setErr('Admin: failed to create chapter ! [' . $chapterClass->getTitle() . ']'); }
-		else { Error_manager::setErr('Création impossible pour : "' . htmlspecialchars($_POST['admNewChapter']) . '"'); }
+		else { Error_manager::setErr('Création impossible pour : "' . $admNewChapter . '"'); }
+		$admNewChapter = null;
 	}
 	else if (isset($_POST['admDelChapter']))
 	{
