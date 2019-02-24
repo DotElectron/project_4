44c4fabe256420fb08c2faa2c4b8fbcf7b843e63
DotElectron <baal_therm.electron@live.fr>
2019-02-25 00:22:17 +0100

    Update index.php
    - Prepare ref to TinyMCE (ext js)
    & (int js) pack "fr_FR"

--- index.php
+++ index.php
@@ -41,7 +41,6 @@ else { Error_manager::setErr('Error: session is down !'); }
     <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
     <link rel="icon" href="favicon.png" type="image/png">
     <!-- External CSS Links -->
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/solid.css" integrity="sha384-rdyFrfAIC05c5ph7BKz3l5NG5yEottvO/DQ0dCrwD8gzeQDjYBHNr1ucUpQuljos" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/fontawesome.css" integrity="sha384-u5J7JghGz0qUrmEsWzBQkfvc8nK3fUT7DCaQzNQ+q4oEXhGSx+P2OqjWsfIRB8QT" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Anonymous+Pro:400,700">
@@ -49,14 +48,12 @@ else { Error_manager::setErr('Error: session is down !'); }
     <link rel="stylesheet" type="text/css" href="public/css/adv-flex-style-min.css">
     <link rel="stylesheet" type="text/css" href="public/css/style.css">
     <!-- External JavaScript -->
-    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script> -->
+    <script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=0j0otcebzd9e4f92cv5nw7zzzyyt9ihnb6sbvkeeoexb3tws"></script>
     <!-- Internal JavaScript -->
-    <!-- <script src="public/js/anim.js"></script> -->
     <script src="public/js/main.js"></script>
     <script src="public/js/navbar.js"></script>
     <script src="public/js/admin.js"></script>
+    <script src="public/js/tiny-mce--fr.js"></script>
 </head>
 
 <body onload="body_adjustement()">
